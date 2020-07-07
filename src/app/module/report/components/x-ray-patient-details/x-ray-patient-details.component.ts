0a2c4bad9c1727b7fee767a3e437e55fd0b134b7
Pramoda K M <pramoda.km@ltts.com>
2020-07-07 20:48:48 +0530

    WIP: Report page design started

--- src/app/module/report/components/x-ray-patient-details/x-ray-patient-details.component.ts
+++ src/app/module/report/components/x-ray-patient-details/x-ray-patient-details.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cxr-x-ray-patient-details',
   templateUrl: './x-ray-patient-details.component.html',
-  styleUrls: ['./x-ray-patient-details.component.scss']
+  styleUrls: ['./x-ray-patient-details.component.scss'],
 })
 export class XRayPatientDetailsComponent implements OnInit {
+  findings = [
+    'Lungs are clear, no evidence of pulmonary parenchymal masses or consolidations.',
+    'Normal hilar vascular markings',
+    'both costophrenic angles are clear',
+    'there is cardiomegaly',
+    'the mediastinum is within normal limits',
+    'Normal hilar vascular markings',
+    'both costophrenic angles are clear',
+    'there is cardiomegaly',
+    'the mediastinum is within normal limits',
+  ];
 
-  constructor() { }
+  impressions = [
+    'Cardiomegaly',
+    'Lung Lesion',
+    'Cardiomegaly',
+    'Cardiomegaly',
+    'Lung Lesion',
+    'Cardiomegaly',
+  ];
 
-  ngOnInit(): void {
-  }
+  comments = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
+  clinicalHistory = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/module/report/report.module.ts
+++ src/app/module/report/report.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReportComponent } from './report.component';
 import { XRayPatientDetailsComponent } from './components/x-ray-patient-details/x-ray-patient-details.component';
 import { XRayPatientImageComponent } from './components/x-ray-patient-image/x-ray-patient-image.component';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
-  declarations: [ReportComponent, XRayPatientDetailsComponent, XRayPatientImageComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ReportComponent,
+    XRayPatientDetailsComponent,
+    XRayPatientImageComponent,
+  ],
+  imports: [CommonModule, MaterialModule],
 })
-export class ReportModule { }
+export class ReportModule {}
