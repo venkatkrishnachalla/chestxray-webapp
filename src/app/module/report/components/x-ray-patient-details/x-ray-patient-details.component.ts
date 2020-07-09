666e97a5ad9e69b8fb2485deb15684f3761c3f8b
Pramoda K M <pramoda.km@ltts.com>
2020-07-09 12:28:20 +0530

    Patient details binded in report page

--- src/app/module/report/components/x-ray-patient-details/x-ray-patient-details.component.ts
+++ src/app/module/report/components/x-ray-patient-details/x-ray-patient-details.component.ts
@@ -14,17 +14,36 @@ export class XRayPatientDetailsComponent implements OnInit {
     'the mediastinum is within normal limits',
   ];
 
-  impressions = [
-    'Cardiomegaly',
-    'Lung Lesion',
-    'Cardiomegaly',
-    'Cardiomegaly',
-  ];
+  impressions = ['Cardiomegaly', 'Lung Lesion', 'Cardiomegaly', 'Cardiomegaly'];
 
-  comments = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
-  clinicalHistory = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
+  // age: number;
+  // birthDate: '1984-05-02T00:00:00';
+  // hospitalPatientId: '1000';
+  // id: 'e3cbba88-83fe746c-6e35783c-9404b4bc-0c7ee9eb';
+  // lastUpdate: '2020-07-07T08:44:25';
+  // name: 'Salman';
+  // referringPhysicianName: 'Mohan';
+  // sex: 'M';
+  // status: false;
+
+  age: number;
+  dateOfCreation: string;
+  hospitalPatientId: string;
+  status: boolean;
+  name: string;
+  sex: string;
+  time: string;
+  referringPhysicianName: string;
+  patientInfo: any;
+
+  comments =
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
+  clinicalHistory =
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+     this.patientInfo = JSON.parse(sessionStorage.getItem('patientDetail'));
+  }
 }
