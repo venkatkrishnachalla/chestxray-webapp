1262896b967f5fac6e178d15e843f2e9c7ba874e
pramodkmsp <pramodkmsp1995@gmail.com>
2020-08-21 14:16:26 +0530

    Removed x-ray-annotated-data value stored in a session while navigating to dashboard page

--- src/app/module/home/patient-list/patient-list.component.ts
+++ src/app/module/home/patient-list/patient-list.component.ts
@@ -55,6 +55,7 @@ export class PatientListComponent implements OnInit {
 
   /*** class init function ***/
   ngOnInit() {
+    sessionStorage.removeItem('x-ray_Data');
     this.overlayNoRowsTemplate = 'No Data Available';
     this.showError = false;
     this.defaultColDef = { width: 200 };
