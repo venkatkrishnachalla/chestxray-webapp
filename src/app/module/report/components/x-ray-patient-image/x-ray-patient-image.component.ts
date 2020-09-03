71acc3778e621570467e44bce2a1d3faec2608e4
skashok <ashok.kanmani@ltts.com>
2020-09-03 11:43:22 +0530

    #mail to functionality

--- src/app/module/report/components/x-ray-patient-image/x-ray-patient-image.component.ts
+++ src/app/module/report/components/x-ray-patient-image/x-ray-patient-image.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { XRayService } from 'src/app/service/x-ray.service';
 import { PatientDetailData } from 'src/app/module/auth/interface.modal';
 import { fabric } from 'fabric';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/module/auth/auth.service';
+import User from 'src/app/module/auth/user.modal';
 @Component({
   selector: 'cxr-x-ray-patient-image',
   templateUrl: './x-ray-patient-image.component.html',
   styleUrls: ['./x-ray-patient-image.component.scss'],
 })
+
 // XRayPatientImageComponent class implementation
 export class XRayPatientImageComponent implements OnInit {
   patientImage: any;
@@ -14,19 +18,27 @@ export class XRayPatientImageComponent implements OnInit {
   xrayAnnotatedImpression: string;
   patientInfo: PatientDetailData;
   pdfTitle: string;
+  isHospitalRadiologist: boolean;
+  userSubscription: Subscription;
+  mailText: string;
+  emailBody: string;
   @Output() printEvent = new EventEmitter();
+  @Output() shareEvent = new EventEmitter();
 
-  /*  
-* constructor for XRayPatientImageComponent class  
-*/ 
-  constructor(private annotatedXrayService: XRayService) {}
+  /*
+   * constructor for XRayPatientImageComponent class
+   */
+  constructor(
+    private annotatedXrayService: XRayService,
+    private authService: AuthService
+  ) {}
 
-/**  
-* This is a init function.  
-* @param {void} empty - A empty param  
-* @example  
-* ngOnInit();
-*/ 
+  /**
+   * This is a init function.
+   * @param - A empty param
+   * @example
+   * ngOnInit();
+   */
   ngOnInit(): void {
     this.patientInfo = history.state.patientDetails;
     if (this.patientInfo === undefined) {
@@ -47,15 +59,39 @@ export class XRayPatientImageComponent implements OnInit {
           this.xrayAnnotatedImage = image;
         }
       });
+    this.userSubscription = this.authService.userSubject.subscribe(
+      (user: User) => {
+        if (user) {
+          this.isHospitalRadiologist =
+            user.userroles[0] === 'HospitalRadiologist' ? true : false;
+        }
+      }
+    );
   }
 
-  /**  
-* This is a print button click emit function.  
-* @param {void} empty - A empty param  
-* @example  
-* printClick();
-*/ 
+  /**
+   * This is a print button click emit function.
+   * @param - A empty param
+   * @example
+   * printClick();
+   */
   printClick() {
     this.printEvent.emit(true);
+  }
+
+  /**
+   * This is a shareButtonEvent button click emit function.
+   * @param - A empty param
+   * @example
+   * shareButtonEvent();
+   */
+  shareButtonEvent() {
+    this.shareEvent.emit(true);
+    const formattedBody =
+      'X-ray Report for patient: ' + this.patientInfo.name + '\n' + '\n';
+    const mailToLink =
+      'mailto:?subject=Chest-rAi-Report&body=' +
+      encodeURIComponent(formattedBody);
+    location.href = mailToLink;
   }
 }
--- src/app/module/report/report.component.ts
+++ src/app/module/report/report.component.ts
@@ -1,40 +1,83 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { EventEmitterService } from '../../service/event-emitter.service';
-import { PatientDetailData, InvokeReportData } from '../auth/interface.modal';
+import {
+  PatientDetailData,
+  InvokeReportData,
+  ImpressionData,
+} from '../auth/interface.modal';
 import { CanvasImageComponent } from '../x-ray/component/canvas-image/canvas-image.component';
 import { ImpressionComponent } from '../x-ray/component/impression/impression.component';
 import { FindingsComponent } from '../x-ray/component/findings/findings.component';
 import { SpinnerService } from '../shared/UI/spinner/spinner.service';
-
+import { staticContentHTML } from 'src/app/constants/staticContentHTML';
+import { ToastrService } from 'ngx-toastr';
+declare var jsPDF: any;
 @Component({
   selector: 'cxr-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss'],
 })
-// ReportComponent class implementation  
+// ReportComponent class implementation
 export class ReportComponent implements OnInit {
   patientInfo: PatientDetailData;
   showPrintForm = false;
+  showPrintFormPdf = false;
+  annotatedImage: string;
+  status: string;
+  pdfTitle: string;
+  reportPageText: {
+    patientDetails: string;
+    clinicalHistory: string;
+    impressions: string;
+    findings: string;
+    commentsAndRecommendations: string;
+  };
+  clinicalHistory: any;
+  annotatedImpression: ImpressionData;
+  pdfFindings: string;
+  pdfComments: string;
+  cxrPrintHeaderName = 'CXR Radiological Report';
   @ViewChild(CanvasImageComponent) canvas: CanvasImageComponent;
   @ViewChild(ImpressionComponent) impressions: ImpressionComponent;
   @ViewChild(FindingsComponent) findings: FindingsComponent;
-  
-/*  
-* constructor for ReportComponent class  
-*/ 
+  @ViewChild('content', { static: false }) content: ElementRef;
+  readonly constants = staticContentHTML;
+
+  /*
+   * constructor for ReportComponent class
+   */
+
   constructor(
     private router: Router,
     private eventEmitterService: EventEmitterService,
-    private spinnerService: SpinnerService
-  ) {}
+    private spinnerService: SpinnerService,
+    private changeDetector: ChangeDetectorRef,
+    private toastrService: ToastrService
+  ) {
+    this.eventEmitterService.commentSubject.subscribe((data) => {
+      this.pdfComments = data;
+      this.changeDetector.markForCheck();
+    });
+    this.eventEmitterService.findingsSubject.subscribe((data) => {
+      this.pdfFindings = data;
+      this.changeDetector.markForCheck();
+    });
+  }
 
-/**  
-* This is a init function.  
-* @param {void} empty - A empty param  
-* @example  
-* ngOnInit();
-*/ 
+  /**
+   * This is a init function.
+   * @param {void} empty - A empty param
+   * @example
+   * ngOnInit();
+   */
+
   ngOnInit(): void {
     this.spinnerService.show();
     this.showPrintForm = false;
@@ -49,17 +92,34 @@ export class ReportComponent implements OnInit {
         }
       }
     );
+    this.reportPageText = this.constants.reportPage;
+    this.patientInfo = history.state.patientDetails;
+    this.annotatedImage = sessionStorage.getItem('annotatedImage');
+    if (this.patientInfo === undefined) {
+      const patientInfo = JSON.parse(sessionStorage.getItem('patientDetail'));
+      this.patientInfo = patientInfo;
+    }
+    if (this.patientInfo.status === false) {
+      this.status = 'Drafted';
+    } else {
+      this.status = 'Unreported';
+    }
     setTimeout(() => {
       this.spinnerService.hide();
     }, 2500);
   }
 
-  /**  
-* This is a  event to go back to xray page .  
-* @param {void} empty - A empty param  
-* @example  
-* goBackToXray();
-*/ 
+  impressionData(event: any) {
+    this.annotatedImpression = event;
+  }
+
+  /**
+   * This is a  event to go back to xray page .
+   * @param {void} empty - A empty param
+   * @example
+   * goBackToXray();
+   */
+
   goBackToXray() {
     this.eventEmitterService.onComponentButtonClick({
       data: [],
@@ -70,13 +130,39 @@ export class ReportComponent implements OnInit {
     });
   }
 
-/**  
-* This is a event to enable print preview selector.  
-* @param {string} value - A string param 
-* @example  
-* enablePrint(event);
-*/ 
+  /**
+   * This is a event to enable print preview selector.
+   * @param {string} value - A string param
+   * @example
+   * enablePrint(event);
+   */
+
   enablePrint(event) {
     this.showPrintForm = event;
+  }
+
+  makePdf(event) {
+    this.spinnerService.show();
+    this.showPrintFormPdf = event;
+    const timestamp = Number(new Date());
+    const hospitalPatientId = this.patientInfo.hospitalPatientId
+      ? this.patientInfo.hospitalPatientId
+      : this.patientInfo.name;
+    this.pdfTitle = hospitalPatientId + '_' + timestamp;
+    setTimeout(() => {
+      const doc = new jsPDF();
+      doc.addHTML(this.content.nativeElement, () => {
+        doc.save(this.pdfTitle + '.pdf');
+        this.toastrService.success(
+          'filename: ' + this.pdfTitle + '.pdf',
+          'X-ray report saved to downloads folder',
+          {
+            timeOut: 3000,
+          }
+        );
+        this.showPrintFormPdf = false;
+        this.spinnerService.hide();
+      });
+    }, 250);
   }
 }
