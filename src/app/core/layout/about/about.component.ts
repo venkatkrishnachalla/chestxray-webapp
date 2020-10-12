e6576aa529af48bd340bf767ba2a25e00c31a966
Mallipeddi Gokul Kiriti <Mallipeddi.Kiriti@Ltts.com>
2020-10-12 10:59:02 +0530

    carousel-updates

--- src/app/core/layout/about/about.component.ts
+++ src/app/core/layout/about/about.component.ts
@@ -5,7 +5,7 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'cxr-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
   currentApplicationVersion = environment.appVersion;
@@ -15,25 +15,44 @@ export class AboutComponent implements OnInit {
 
   // Slider Images
   slides = [
-    {image: '../../../../assets/images/select icon active@2x.png'}, 
-    {image: 'https://gsr.dev/material2-carousel/assets/demo.png'},
-    {image: 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
-    {image: 'https://gsr.dev/material2-carousel/assets/demo.png'}
+    {
+      image: '../../../../assets/images/select icon active@2x.png',
+      title: 'Title Name',
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    },
+    {
+      image: 'https://gsr.dev/material2-carousel/assets/demo.png',
+      title: 'Title Name',
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    },
+    {
+      image: 'https://gsr.dev/material2-carousel/assets/demo.png',
+      title: 'Title Name',
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    },
+    {
+      image: 'https://gsr.dev/material2-carousel/assets/demo.png',
+      title: 'Title Name',
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    },
   ];
 
-  // Slider content
-  slideContent = [
-    {content: 'Hello Pramod'},
-    {content: 'Hello Pramod'},
-    {content: 'Hello Pramod'},
-    {content: 'Hello Pramod'},
-  ];
+  // // Slider content
+  // slideContent = [
+  //   {content: 'Hello Pramod'},
+  //   {content: 'Hello Pramod'},
+  //   {content: 'Hello Pramod'},
+  //   {content: 'Hello Pramod'},
+  // ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.copyRightText = this.constants.copyRight;
     this.socialMediaIcons = this.constants.socialMedia;
   }
-
 }
