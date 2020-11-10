c5b9247549dd6a99a56bee0111db2e81ccc8783b
Lakshmichandrappa <Lakshmi.C@ltts.com>
2020-11-10 20:32:17 +0530

    changed stroke width from 2 to 3

--- src/app/module/x-ray/component/canvas-image/canvas-image.component.ts
+++ src/app/module/x-ray/component/canvas-image/canvas-image.component.ts
@@ -1350,7 +1350,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
             // @ts-ignore
             disease: disease.disease,
             stroke: disease.color,
-            strokeWidth: 2,
+            strokeWidth: 3,
             fill: '',
             originX: 'center',
             originY: 'center',
@@ -1409,7 +1409,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
             this.canvas.add(
               new fabric.Path(this.coordinateList.join(' '), {
                 stroke: disease.color,
-                strokeWidth: 2,
+                strokeWidth: 3,
                 fill: '',
                 originX: 'center',
                 originY: 'center',
@@ -1487,7 +1487,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
         angle: diseaseItem.r,
         stroke: diseaseItem.color,
         strokeDashArray: diseaseItem.strokeDashArray,
-        strokeWidth: 2,
+        strokeWidth: 3,
         fill: '',
         selectable: true,
         strokeUniform: true,
@@ -1521,7 +1521,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
               height: 0,
               left: pointer.x,
               top: pointer.y,
-              strokeWidth: 2,
+              strokeWidth: 3,
               stroke: '#ffff00',
               fill: '',
               selectable: true,
@@ -2172,7 +2172,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
       this.enableDrawEllipseMode = false;
       this.canvas.isDrawingMode = true;
       this.canvas.freeDrawingBrush.color = '#ffff00';
-      this.canvas.freeDrawingBrush.width = 4;
+      this.canvas.freeDrawingBrush.width = 3;
       this.canvas.freeDrawingBrush.strokeUniform = true;
       this.canvas.observe('mouse:move', (e) => {
         const pointer = this.canvas.getPointer(e.e);
@@ -2613,7 +2613,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
           id: element.id,
           originX: 'center',
           originY: 'center',
-          strokeWidth: 2,
+          strokeWidth: 3,
           fill: '',
           selectable: true,
           strokeDashArray: element.strokeDashArray,
@@ -2658,7 +2658,7 @@ export class CanvasImageComponent implements OnInit, OnDestroy {
           new fabric.Path(this.coordinateList.join(' '), {
             // @ts-ignore
             stroke: element.color,
-            strokeWidth: 2,
+            strokeWidth: 3,
             fill: '',
             originX: 'center',
             originY: 'center',
